b36e39bca294c7d0374256f1467b82dc45b9418d
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-01-05 00:32:33 +0100

    fonctionel et propre(je crois) le button s'est fait oneshoot

--- app.js
+++ app.js
@@ -10,16 +10,20 @@ function main(){
 	vous pouvez biensur créer d'autres fonctions si nécessaire
 	*/
 
-	// 1. placez un listener sur le click des éléments <button>
+	$('button').click(function(){
+		var esp = $(this).attr("data-animal");//creer une var avec l'attribut "data-animal" de l'element "visé" (soit cat, bear ou fish)
+		console.log(typeof(species[esp])); //on console.log de plein de façons differente pour tester (genre "typeof species.esp") qui marche pas.
+		$("#holder").html( "<img src='img/" + species[esp] + "'>");	// une foi qu'on a la bonne on le rentre dans un bout
+	});																// de html qu'on inject avec .html(""). (un peu comme innerHTML)
+
+
+	// 1. placez un listener sur le click des éléments <button>								<<  GG'  >>
 	// 2. dans le callback récupérez le data attribute animal
 	// 3. récupérerez dans l'objet species la valeur correspond à l'attribut récupéré
 	// 4. ajoutez élément img en assignant à son attibut src la valeur précédemment récupérée
 	// 5. inserez cet élément dans l'élément ayant l'id holder
 	// 
 	// Conseils : avancez étape par étape, console.log et debugger autant que nécessaires !
-	
-	
-	 
 }
 
 $(document).ready(function(){
